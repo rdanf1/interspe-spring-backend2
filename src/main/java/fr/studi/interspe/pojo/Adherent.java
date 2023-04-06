06a15a2d38dc43fad5c4e979e675080090743bf0
rdanf1 <reylet.daniel@gmail.com>
2023-04-07 01:07:23 +0200

    Some docs - Config proj struct = proj Settings

--- src/main/java/fr/studi/interspe/pojo/Adherent.java
+++ src/main/java/fr/studi/interspe/pojo/Adherent.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+/* import jakarta.validation.constraints.NotNull; */
 import lombok.Getter;
 import lombok.Setter;
 @Entity
